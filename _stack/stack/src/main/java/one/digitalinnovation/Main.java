aff249a366920edd62b1fff2301024ff61b2cbef
Neto <franciscomdlneto@gmail.com>
2023-04-19 19:34:07 -0300

    Stack

---
+++ _stack/stack/src/main/java/one/digitalinnovation/Main.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Carro> stackCarros = new Stack<>();
+        stackCarros.push(new Carro("Ford"));
+        stackCarros.push(new Carro("Fiat"));
+        stackCarros.push(new Carro("Volkswagen"));
+        stackCarros.push(new Carro("KIA"));
+        System.out.println(stackCarros);
+        System.out.println(stackCarros.pop());
+        System.out.println(stackCarros.peek());
+    }
+}
